4c0f4354a71dbd43db39e8211754867013079ee1
Vincent Sisk <vinnysisk@gmail.com>
2019-08-17 22:49:32 -0600

    add "natural title bar"

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -326,6 +326,12 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+
+  (add-to-list 'default-frame-alist
+               '(ns-transparent-titlebar . t))
+
+  (add-to-list 'default-frame-alist
+               '(ns-appearance . dark))
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
